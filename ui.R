51c1866bea9c68635790a53c63c7e13dc584806f
Bán Miklós <banm@vocs.unideb.hu>
2018-04-16 11:30:53 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,14 +3,19 @@ library(shiny)
 shinyUI(fluidPage(
   
   headerPanel("OpenRingMaps Species Density"),
+  sliderInput("obs", 
+                "Number of observations:", 
+                min = 1,
+                max = 1000, 
+                value = 500),
 
-  sidebarPanel(
-    selectInput("species",
+  #sidebarPanel(
+  #  selectInput("species",
                 "Species:", c(df$species))),
    
-  sidebarPanel(
-    selectInput("breaks","Breaks of histogram:",c("days", "weeks", "months"))
-  ),
+  #sidebarPanel(
+  #  selectInput("breaks","Breaks of histogram:",c("days", "weeks", "months"))
+  #),
 
   mainPanel(
     plotOutput("distPlot")
