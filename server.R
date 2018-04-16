623415d912bff4c27fac07228a5aa6b8d588a9bb
Marika <j.marika1118@gmail.com>
2018-04-16 11:59:55 +0200

    postgres kiiktatva
    hogy működjön a példa helyben

--- server.R
+++ server.R
@@ -1,18 +1,18 @@
 library(shiny)
-library(RPostgreSQL)
+#library(RPostgreSQL)
 
 shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
-        drv <- dbDriver("PostgreSQL")
-        con <-dbConnect(drv, dbname = database,
-                 host = server, port = port,
-                 user = puser, password = ppass)
-        df <- dbGetQuery(con, "SELECT * FROM ring 
-                                  WHERE datum > '2015-07-11' AND datum < '2015-10-31'
-                                  ")
-        dbDisconnect(con)
+        #drv <- dbDriver("PostgreSQL")
+        #con <-dbConnect(drv, dbname = database,
+        #         host = server, port = port,
+        #         user = puser, password = ppass)
+        #df <- dbGetQuery(con, "SELECT * FROM ring 
+        #                          WHERE datum > '2015-07-11' AND datum < '2015-10-31'
+        #                          ")
+        #dbDisconnect(con)
 
-        hist(df$datum[df$species == c(input$species)], c(input$breaks))
+        #hist(df$datum[df$species == c(input$species)], c(input$breaks))
         dist <- rnorm(input$obs)
         hist(dist)
 
