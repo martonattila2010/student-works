c8e3029175bec3be2eafb37bc5b25a22b409c40b
Bán Miklós <banm@vocs.unideb.hu>
2018-05-02 09:53:32 +0200

    Update run_server.R

--- run_server.R
+++ run_server.R
@@ -1,6 +1,6 @@
 # load shiny lib
 library(shiny)
 
-# run shiny server on the given port at http://localhost/.../shiny/
+# run shiny server on the given port at http://localhost:7901/.../shiny/
 # from ./shiny directory
 runApp("./",7901)
