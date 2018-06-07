b34d14b276f1229db59f9d9cad84877a7b1e39c9
Bán Miklós <banm@vocs.unideb.hu>
2018-06-07 10:52:11 +0200

    Add files via upload
    Adatbázis kapcsolattal működik OpenBioMaps szerverekről futtatva

--- server.R
+++ server.R
@@ -1,23 +1,73 @@
 library(shiny)
-#library(RPostgreSQL)
+
+options(encoding = 'UTF-8')
+Sys.setlocale(category = 'LC_ALL','en_US.UTF-8')
+
+# Database connection
+library(RPostgreSQL)
+dbport <- 5432
+# variables from run_server.R (OpenBioMaps Rscript exec())
+# sql user name:
+#   puser
+# sql user password:
+#   ppass
+# sql project table:
+#   project
+# sql database name:
+#   database
+# sql server:
+#   localhost
+# sql server port:
+#   dbport
+drv <- dbDriver("PostgreSQL")
+con <-dbConnect(drv, dbname = database,
+                 host = server, port = dbport,
+                 user = puser, password = ppass)
 
 shinyServer(function(input, output) {
-  output$distPlot <- renderPlot({
-        #drv <- dbDriver("PostgreSQL")
-        #con <-dbConnect(drv, dbname = database,
-        #         host = server, port = port,
-        #         user = puser, password = ppass)
-        #df <- dbGetQuery(con, "SELECT * FROM ring 
-        #                          WHERE datum > '2015-07-11' AND datum < '2015-10-31'
-        #                          ")
-        #dbDisconnect(con)
 
-        #hist(df$datum[df$species == c(input$species)], c(input$breaks))
-        dist <- rnorm(input$obs)
-        hist(dist)
+    mydb <- reactive({
+        if (length(input$gyujto)) {
+            # returning
+            paste(" AND gyujto='",input$gyujto,"'",sep='')
+        }
+    })
+    output$text <- renderText({  
+        # returning
+        paste("You have selected:",input$gyujto)
+    })  
 
+    sp <- reactive({
+        cmd <- paste("SELECT obm_id,obm_datum as datum,faj as species FROM",project,"WHERE obm_datum > '2010-07-11' AND obm_datum < '2018-10-31' ",mydb())
+        MyDf <- dbGetQuery(con, cmd)
+        # returning
+        MyDf$species
+    })
+
+    output$speciesList <- renderUI({
+        selectInput('species', label='Fajok', choices=sp())
+    })
+
+    output$gyujto <- renderUI({
+        cmd <- paste("SELECT DISTINCT uploader_name FROM uploadings")
+        MyDf <- dbGetQuery(con, cmd)
+        # returning
+        selectInput('gyujto', label='Gyűjtők', choices=MyDf$uploader_name)
+    })
+
+
+    output$distPlot <- renderPlot({
+
+        cmd <- paste("SELECT TO_CHAR(obm_datum,'YYYY-MM-DD') as datum FROM ",project," WHERE faj='",input$species,"'",sep='')
+        MyDf <- dbGetQuery(con, cmd)
+
+        #hist(MyDf$datum[MyDf$species == c(input$species)], c(input$breaks))
+        hist(MyDf$datum)
+        
 
     })
+    
 })
 
 
+    #on.exit(dbDisconnect(con), add = TRUE)
--- ui.R
+++ ui.R
@@ -1,24 +1,38 @@
 library(shiny)
 
+options(encoding = 'UTF-8')
+Sys.setlocale(category = 'LC_ALL','en_US.UTF-8')
+
+# Define UI for application that plots something
 shinyUI(fluidPage(
-  
-  headerPanel("OpenRingMaps Species Density"),
-  sliderInput("obs", 
-              "Number of observations:", 
-              min = 1,
-              max = 1000, 
-              value = 500),
-  
-# sidebarPanel(
-  # selectInput("species",
- #  "Species:", c(df$species)),
 
-#sidebarPanel(
- # selectInput("breaks","Breaks of histogram:",c("days", "weeks", "months"))
-#),
+    #headerPanel(paste(project," species density")),
+    titlePanel("OBM stats"),
 
-mainPanel(
-  plotOutput("distPlot")
-)
+    sidebarPanel(
+        selectInput("state", "Choose a sevice:",
+          list(`Last month` = c("All uploads", "My data", "..."),
+               `This month` = c("All uploads", "My data", "..."),
+               `Last year` = c("All uploads", "My data", "..."),
+               `This year` = c("All uploads", "My data", "..."))
+        ),
+
+        uiOutput("gyujto"),
+
+        textOutput("text"),
+        
+        uiOutput('speciesList')
+    ),
+    mainPanel(
+        plotOutput("distPlot")
+    )
+
+    #sidebarPanel(
+    #    selectInput("Hbreaks", label = "Breaks of histogram:",choices = c("days", "weeks", "months"))
+    #),
+
+    #mainPanel(
+    #    plotOutput("distPlot")
+    #)
 ))
-#gyakorlasképpen koment
+
